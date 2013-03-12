68133e19f5ebc7a7b3b9ee335853d09fc023b5bf
Matt <thompsonson@gmail.com>
2013-03-12 16:45:38 +0000

    Initial Commit
    Console app to get things up and running

---
+++ OfxToMmexConsoleApp/Model.cs
@@ -0,0 +1,80 @@
+﻿using PetaPoco;
+using System.Collections.Generic;
+
+namespace OfxToMmexConsoleApp
+{
+    [PetaPoco.TableName("ACCOUNTLIST_V1")]
+    [PetaPoco.PrimaryKey("ACCOUNTID")]
+    [PetaPoco.ExplicitColumns]
+    public class Accounts 
+    {
+
+        [PetaPoco.Column] public int ACCOUNTID { get; set; }
+        [PetaPoco.Column] public string ACCOUNTNAME { get; set; }
+        [PetaPoco.Column] public string ACCOUNTTYPE { get; set; }
+        [PetaPoco.Column] public string ACCOUNTNUM { get; set; }
+        [PetaPoco.Column] public string STATUS { get; set; }
+        [PetaPoco.Column] public string NOTES { get; set; }
+        [PetaPoco.Column] public string HELDAT { get; set; }
+        [PetaPoco.Column] public string WEBSITE { get; set; }
+        [PetaPoco.Column] public string CONTACTINFO { get; set; }
+        [PetaPoco.Column] public string ACCESSINFO { get; set; }
+        [PetaPoco.Column] public long INITIALBAL { get; set; }
+        [PetaPoco.Column] public string FAVORITEACCT { get; set; }
+        [PetaPoco.Column] public int CURRENCYID { get; set; }
+
+    }
+
+    
+    [PetaPoco.TableName("CURRENCYFORMATS_V1")]
+    [PetaPoco.PrimaryKey("CURRENCYID")]
+    [PetaPoco.ExplicitColumns]
+    public class Currencies 
+    {
+        [PetaPoco.Column] public int CURRENCYID { get; set; }
+        [PetaPoco.Column] public string CURRENCYNAME { get; set; }
+        [PetaPoco.Column] public string PFX_SYMBOL { get; set; }
+        [PetaPoco.Column] public string SFX_SYMBOL { get; set; }
+        [PetaPoco.Column] public string DECIMAL_POINT { get; set; }
+        [PetaPoco.Column] public string GROUP_SEPARATOR { get; set; }
+        [PetaPoco.Column] public string UNIT_NAME { get; set; }
+        [PetaPoco.Column] public string CENT_NAME { get; set; }
+        [PetaPoco.Column] public string SCALE { get; set; }
+        [PetaPoco.Column] public long BASECONVRATE { get; set; }
+        [PetaPoco.Column] public string CURRENCY_SYMBOL { get; set; }
+    }
+
+    [PetaPoco.TableName("CHECKINGACCOUNT_V1")]
+    [PetaPoco.PrimaryKey("TRANSID")]
+    [PetaPoco.ExplicitColumns]
+    public class Transactions 
+    {
+        [PetaPoco.Column] public int TRANSID { get; set; }
+        [PetaPoco.Column] public int ACCOUNTID { get; set; }
+        [PetaPoco.Column] public int TOACCOUNTID { get; set; }
+        [PetaPoco.Column] public int PAYEEID { get; set; }
+        [PetaPoco.Column] public string TRANSCODE { get; set; }
+        [PetaPoco.Column] public decimal TRANSAMOUNT { get; set; }
+        [PetaPoco.Column] public string STATUS { get; set; }
+        [PetaPoco.Column] public string TRANSACTIONNUMBER { get; set; }
+        [PetaPoco.Column] public string NOTES { get; set; }
+        [PetaPoco.Column] public int CATEGID { get; set; }
+        [PetaPoco.Column] public int SUBCATEGID { get; set; }
+        [PetaPoco.Column] public string TRANSDATE { get; set; }
+        [PetaPoco.Column] public int FOLLOWUPID { get; set; }
+        [PetaPoco.Column] public double TOTRANSAMOUNT { get; set; }
+        [PetaPoco.Column] public string FITID { get; set; }
+        
+    }
+    
+    [PetaPoco.TableName("PAYEE_V1")]
+    [PetaPoco.PrimaryKey("PAYEEID")]
+    [PetaPoco.ExplicitColumns]
+    public class Payee 
+    {
+        [PetaPoco.Column] public int PAYEEID { get; set; }
+        [PetaPoco.Column] public string PAYEENAME { get; set; }
+        [PetaPoco.Column] public int CATEGID { get; set; }
+        [PetaPoco.Column] public int SUBCATEGID { get; set; }
+    }
+}
---
+++ OfxToMmexConsoleApp/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OFXSharp;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace OfxToMmexConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Into Main");
+
+            OFXDocumentParser doc = new OFXDocumentParser();
+
+            OFXDocument OfxDocument = new OFXDocument();
+
+            OfxDocument = doc.Import(new FileStream("D:\\Dropbox\\Projects\\myBank\\data\\20130311 - Barclays.qbo", FileMode.Open));
+            //OfxDocument = doc.Import(new FileStream("D:\\Amex.qfx", FileMode.Open));
+
+            Console.WriteLine("File imported");
+
+            // Create a PetaPoco database object
+            var db = new PetaPoco.Database("mmex_db");
+            Console.WriteLine("PetPoco DB started");
+
+            // Show all Accounts    
+            foreach (var a in db.Query<Accounts>("SELECT * FROM ACCOUNTLIST_V1;"))
+            {
+                Console.WriteLine("{0} - {1} - {2}", a.ACCOUNTID, a.ACCOUNTNUM, a.ACCOUNTNAME);
+            }
+
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine(OfxDocument.Statements.Count() + " statements in the ofx file");
+            Console.WriteLine("-------------------------------------------------------------------------");
+
+            foreach (OFXStatement ofxstatment in OfxDocument.Statements)
+            {
+                Console.WriteLine("Account # " + ofxstatment.Account.AccountID);
+
+                var account = db.SingleOrDefault<Accounts>("SELECT * FROM ACCOUNTLIST_V1 WHERE ACCOUNTNUM=@0", ofxstatment.Account.AccountID);
+                if (account == null)
+                {
+                    Console.WriteLine("Creating the account");
+                    //get the currency ID
+                    var currency = db.SingleOrDefault<Currencies>("SELECT * FROM CURRENCYFORMATS_V1 WHERE CURRENCY_SYMBOL=@0", ofxstatment.Currency); //(CURRENCY_SYMBOL: document.CURDEF);
+                    // defaulting to GBP if no currency
+                    int currency_id = 6;
+                    if (currency != null)
+                        currency_id = currency.CURRENCYID;
+                    // create the account 
+                    account = new Accounts();
+                    account.ACCOUNTNUM = ofxstatment.Account.AccountID;
+                    account.ACCOUNTTYPE = ofxstatment.Account.AccountType.ToString(); // document.ACCTTYPE;
+                    account.ACCOUNTNAME = "Automated Import of account #" + ofxstatment.Account.AccountID;
+                    account.STATUS = "Open";
+                    account.HELDAT = "Bank ID - " + ofxstatment.Account.BankID;
+                    account.INITIALBAL = 0;
+                    account.FAVORITEACCT = "TRUE";
+                    account.CURRENCYID = currency_id;
+                    // updates ACCOUNTID in NewAccount as well as Inserting the account
+                    db.Insert(account);
+                }
+
+                foreach (OFXSharp.Transaction trans in ofxstatment.Transactions)
+                {
+                    // check if the transaction is already present
+                    Console.WriteLine("Checking for Transaction: " + trans.TransactionID);
+                    var transaction_count = db.SingleOrDefault<long>("SELECT count(*) FROM CHECKINGACCOUNT_V1 WHERE FITID=@0", trans.TransactionID);
+                    if (transaction_count == 0)
+                    {
+                        Console.WriteLine("Inserting Transaction: " + trans.TransactionID);
+                        // get Payee ID and default Categories
+                        Regex re = new Regex(@"REF");
+                        string[] arrPayee = re.Split(trans.Name);
+                        string payeeName = arrPayee[0].Trim();
+                        Console.WriteLine("Checking for Payee: " + payeeName);
+                        var payee = db.SingleOrDefault<Payee>("SELECT * FROM PAYEE_V1 WHERE PAYEENAME=@0", payeeName);
+
+                        if (payee == null)
+                        {
+                            Console.WriteLine("Inserting Payee: " + payeeName);
+                            // create the payee
+                            payee = new Payee();
+                            payee.PAYEENAME = payeeName;
+                            payee.CATEGID = -1;
+                            payee.SUBCATEGID = -1;
+                            // updates PAYEEID in payee as well as Inserting the payee
+                            db.Insert(payee);
+                        }
+
+                        var newTrans = new Transactions();
+                        newTrans.ACCOUNTID = account.ACCOUNTID;
+                        newTrans.PAYEEID = payee.PAYEEID;
+                        newTrans.TRANSCODE = trans.TransType.ToString();
+                        newTrans.TRANSAMOUNT = trans.Amount;
+                        newTrans.STATUS = "F";
+                        newTrans.NOTES = trans.Name;
+                        newTrans.CATEGID = payee.CATEGID;
+                        newTrans.SUBCATEGID = payee.SUBCATEGID;
+                        newTrans.TRANSDATE = trans.Date.ToString() ;
+                        newTrans.FOLLOWUPID = -1;
+                        newTrans.FITID = trans.TransactionID;
+                        db.Insert(newTrans);
+
+                        // **/
+                    } // else do nothing
+                    else
+                    {
+                        Console.WriteLine("Transaction present");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("-------------------------------------------------------------------------");
+
+            // Show all Accounts    
+            foreach (var a in db.Query<Accounts>("SELECT * FROM ACCOUNTLIST_V1;"))
+            {
+                Console.WriteLine("{0} - {1} - {2}", a.ACCOUNTID, a.ACCOUNTNUM, a.ACCOUNTNAME);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
