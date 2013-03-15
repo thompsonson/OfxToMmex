746616a5b76c59cf6960a63d1c4ac2101390f48d
Matt <thompsonson@gmail.com>
2013-03-15 10:30:53 +0000

    added log4net
    very basic console and file logging

--- OfxToMmexConsoleApp/Program.cs
+++ OfxToMmexConsoleApp/Program.cs
@@ -6,36 +6,39 @@ using System.Threading.Tasks;
 using OFXSharp;
 using System.IO;
 using System.Text.RegularExpressions;
+using log4net;
+using log4net.Config;
 
 namespace OfxToMmexConsoleApp
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         private static void ImportOfxToMmex(OFXDocument OfxDocument)
         {
-
             // Create a PetaPoco database object
             var db = new PetaPoco.Database("mmex_db");
-            Console.WriteLine("PetPoco DB started");
+            log.Info("PetPoco DB started");
 
             // Show all Accounts    
             foreach (var a in db.Query<Accounts>("SELECT * FROM ACCOUNTLIST_V1;"))
             {
-                Console.WriteLine("{0} - {1} - {2}", a.ACCOUNTID, a.ACCOUNTNUM, a.ACCOUNTNAME);
+                log.Info(a.ACCOUNTID + " - " + a.ACCOUNTNUM + " - " + a.ACCOUNTNAME);
             }
 
-            Console.WriteLine("-------------------------------------------------------------------------");
-            Console.WriteLine(OfxDocument.Statements.Count() + " statements in the ofx file");
-            Console.WriteLine("-------------------------------------------------------------------------");
+            log.Info("-------------------------------------------------------------------------");
+            log.Info(OfxDocument.Statements.Count() + " statements in the ofx file");
+            log.Info("-------------------------------------------------------------------------");
 
             foreach (OFXStatement ofxstatment in OfxDocument.Statements)
             {
-                Console.WriteLine("Account # " + ofxstatment.Account.AccountID);
+                log.Info("Account # " + ofxstatment.Account.AccountID);
 
                 var account = db.SingleOrDefault<Accounts>("SELECT * FROM ACCOUNTLIST_V1 WHERE ACCOUNTNUM=@0", ofxstatment.Account.AccountID);
                 if (account == null)
                 {
-                    Console.WriteLine("Creating the account");
+                    log.Info("Creating the account");
                     //get the currency ID
                     var currency = db.SingleOrDefault<Currencies>("SELECT * FROM CURRENCYFORMATS_V1 WHERE CURRENCY_SYMBOL=@0", ofxstatment.Currency); //(CURRENCY_SYMBOL: document.CURDEF);
                     // defaulting to GBP if no currency
@@ -66,21 +69,21 @@ namespace OfxToMmexConsoleApp
                 foreach (OFXSharp.Transaction trans in ofxstatment.Transactions)
                 {
                     // check if the transaction is already present
-                    Console.WriteLine("Checking for Transaction: " + trans.TransactionID);
+                    log.Info("Checking for Transaction: " + trans.TransactionID);
                     var transaction_count = db.SingleOrDefault<long>("SELECT count(*) FROM CHECKINGACCOUNT_V1 WHERE FITID=@0", trans.TransactionID);
                     if (transaction_count == 0)
                     {
-                        Console.WriteLine("Inserting Transaction: " + trans.TransactionID);
+                        log.Info("Inserting Transaction: " + trans.TransactionID);
                         // get Payee ID and default Categories
                         Regex re = new Regex(@"REF");
                         string[] arrPayee = re.Split(trans.Name);
                         string payeeName = arrPayee[0].Trim();
-                        Console.WriteLine("Checking for Payee: " + payeeName);
+                        log.Info("Checking for Payee: " + payeeName);
                         var payee = db.SingleOrDefault<Payee>("SELECT * FROM PAYEE_V1 WHERE PAYEENAME=@0", payeeName);
 
                         if (payee == null)
                         {
-                            Console.WriteLine("Inserting Payee: " + payeeName);
+                            log.Info("Inserting Payee: " + payeeName);
                             // create the payee
                             payee = new Payee();
                             payee.PAYEENAME = payeeName;
@@ -115,7 +118,7 @@ namespace OfxToMmexConsoleApp
                     } // else do nothing
                     else
                     {
-                        Console.WriteLine("Transaction present");
+                        log.Info("Transaction present");
                     }
                 }
 
@@ -123,7 +126,7 @@ namespace OfxToMmexConsoleApp
             // Show all Accounts    
             foreach (var a in db.Query<Accounts>("SELECT * FROM ACCOUNTLIST_V1;"))
             {
-                Console.WriteLine("{0} - {1} - {2}", a.ACCOUNTID, a.ACCOUNTNUM, a.ACCOUNTNAME);
+                log.Info(a.ACCOUNTID + " - " + a.ACCOUNTNUM + " - " + a.ACCOUNTNAME);
             }
 
         }
@@ -131,7 +134,11 @@ namespace OfxToMmexConsoleApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Into Main");
+            // Set up a simple configuration that logs on the console.
+           // BasicConfigurator.Configure();
+            XmlConfigurator.Configure(new System.IO.FileInfo(args[0]));
+
+            log.Info("Into Main");
             //create a filesystemwatcher class instance for monitoring a physical file system directory
             FileSystemWatcher watcher = new FileSystemWatcher();
             watcher.Created += new FileSystemEventHandler(watcher_Created);
@@ -140,10 +147,7 @@ namespace OfxToMmexConsoleApp
             //start the monitor
             watcher.EnableRaisingEvents = true;
             
-
-            Console.WriteLine("-------------------------------------------------------------------------");
-
-
+            log.Info("-------------------------------------------------------------------------");
 
             Console.ReadLine();
         }
@@ -152,8 +156,8 @@ namespace OfxToMmexConsoleApp
         {
             var db = new PetaPoco.Database("mmex_db");
             //fullpath
-            Console.WriteLine("-------------------------------------------------------------------------");
-            Console.WriteLine("Importing file: " + e.FullPath);
+            log.Info("-------------------------------------------------------------------------");
+            log.Info("Importing file: " + e.FullPath);
             Workflow wf = new Workflow();
             wf.filename = e.FullPath;
             wf.WatcherCreateTS = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
@@ -164,24 +168,20 @@ namespace OfxToMmexConsoleApp
                 // need to timeout here...
                 
             }
-            // workflow
-            /*
-             *  
-             * */
             wf.ImportStartTS = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             db.Update(wf);
             OFXDocumentParser doc = new OFXDocumentParser();
             OFXDocument OfxDocument = new OFXDocument();
             OfxDocument = doc.Import(new FileStream(e.FullPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None));
-            Console.WriteLine("File has been read");
+            log.Info("File has been read");
             wf.ProcessStartTS = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             wf.DBInsertStartTS = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             db.Update(wf);
             OfxToMmexConsoleApp.Program.ImportOfxToMmex(OfxDocument);
             wf.FinishTS = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             db.Update(wf);
-            Console.WriteLine("Import to Money Manager EX DB complete");
-            Console.WriteLine("-------------------------------------------------------------------------");
+            log.Info("Import to Money Manager EX DB complete");
+            log.Info("-------------------------------------------------------------------------");
         }
 
         private static bool creationComplete(string fileName)
